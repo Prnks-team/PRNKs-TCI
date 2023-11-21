11912dd7ef9db8f93eb8bb8fb8285540cb88660d
prnk1243 <151041466+prnk1243@users.noreply.github.com>
2023-11-21 16:34:53 +0000

    added story

--- aaaa.py
+++ aaaa.py
@@ -1,1 +1,13 @@
+import time
+import os
+import sys
+
 print("Well hello you've seemed to execute the secret command when a pin wasn't added! you're probably a alpha tester reading this and i thank you for taking the time out of your day to test this program and make it better >-<")
+time.sleep(5)
+print("Since you're here I might as well tell you who I am. This is gonna take a long time so sit back and relax the command panel will close after this interaction!")
+time.sleep(10)
+print("""Well you probably already know my name (if you read the copyright) but my name is anthony! I've been programming for about 4 years now and I know: Ruby, Python (ofc), C++, HTML, C#, and much more
+      """)
+print("""I want to take the time to thank my first alpha tester which is my friend Dilbert. He started off as didn't even know what python was but with hours of teaching he is experienced and helped me a lot during this project Thats all i have thank you for listening to my ted talk. """)
+time.sleep(10)
+sys.exit(1)
